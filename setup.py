1cbded08310d8677ba54e7ffec159b8c67580b21
Yan <yans@yancomm.net>
2015-05-27 02:26:06 -0700

    install requirements

--- setup.py
+++ setup.py
@@ -1,2 +1,7 @@
 from distutils.core import setup
-setup(name='archinfo', version='0.01', packages=['archinfo'])
+setup(
+    name='archinfo',
+    version='0.01',
+    packages=['archinfo'],
+    install_requires=[i.strip() for i in open('requirements.txt').readlines() if 'git' not in i]
+)
