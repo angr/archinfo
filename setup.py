36556e6d69e778e0cbbce518f2750383f6e2a6fe
Yan <yans@yancomm.net>
2015-05-27 02:37:02 -0700

    pip installable!

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
     name='archinfo',
-    version='0.01',
+    version='0.02',
     packages=['archinfo'],
-    install_requires=[i.strip() for i in open('requirements.txt').readlines() if 'git' not in i]
+    install_requires=[ 'capstone', 'pyelftools', 'pyvex' ]
 )
