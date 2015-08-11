fa6b1688acfe2072b5f9f8c45d515d0a39f45ac7
Yan <yans@yancomm.net>
2015-08-11 02:10:30 -0700

    bump version number for pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
     name='archinfo',
-    version='0.02',
+    version='0.03',
     packages=['archinfo'],
     install_requires=[ 'capstone', 'pyelftools', 'pyvex' ]
 )
