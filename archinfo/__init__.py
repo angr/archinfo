3220f9a65eef3b8255a72e1c18f40e8ee673db0a
Andrew Dutcher <andrew@andrewdutcher.com>
2015-09-30 12:22:40 -0700

    Moved defines into python dict

--- archinfo/__init__.py
+++ archinfo/__init__.py
@@ -1,14 +1,3 @@
 # pylint: disable=wildcard-import
 from .arch import *
-
-import os as _os
-
-defines = {}
-
-filepath = _os.path.join(_os.path.dirname(_os.path.realpath(__file__)), 'defines.dat')
-for line in open(filepath):
-    line_items = line.split()
-    if len(line_items) == 0:
-        continue
-    defines[line_items[0]] = int(line_items[1], 0)
-
+from .defines import defines
