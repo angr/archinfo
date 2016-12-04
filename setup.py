59a3c07d9c0ceb8c223c9de4a358b89031765999
Andrew Dutcher <andrew@andrewdutcher.com>
2016-12-03 19:32:57 -0800

    use setuptools

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+    from setuptools import find_packages
+    packages = find_packages()
+except ImportError:
+    from distutils.core import setup
+    import os
+    packages = [x.strip('./').replace('/','.') for x in os.popen('find -name "__init__.py" | xargs -n1 dirname').read().strip().split('\n')]
+
 setup(
     name='archinfo',
     version='5.6.12.3',
-    packages=['archinfo'],
+    packages=packages,
     install_requires=[ 'capstone', 'pyelftools' ]
 )
