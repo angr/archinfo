c9e1d1e31fc2f0cc1fe1f90163d5a69431e38e14
wwwzbwcom <zbwhome@outlook.com>
2020-04-28 16:13:55 +0800

    modify code to meet PEP8 requirement

--- archinfo/arch_riscv.py
+++ archinfo/arch_riscv.py
@@ -1,7 +1,7 @@
 try:
     import capstone as _capstone
-    if _capstone.__version__ < "5.0":
-        raise ImportError("Only capstone verison >= 5.0 support RISC-V")
+    if _capstone.__version__ < '5.0':
+        raise ImportError('Only capstone verison >= 5.0 support RISC-V')
 except ImportError:
     _capstone = None
 
@@ -19,22 +19,25 @@ class ArchRISCV(Arch):
     address_types = (int,)
     function_address_types = (int,)
 
-    name = "RISCV"
+    name = 'RISCV'
     bits = 32
-    vex_arch = None # No VEX support
-    qemu_name = None # No Unicorn-engine support
+    vex_arch = None  # No VEX support
+    qemu_name = None  # No Unicorn-engine support
     ida_processor = None
-    triplet = "riscv32-linux-gnu"
+    triplet = 'riscv32-linux-gnu'
     max_inst_bytes = 4
 
     ip_offset = None
     sp_offset = None
     bp_offset = None
     lr_offset = None
-    ret_offset = 11 # a0(x10) For return value
+    ret_offset = 11  # a0(x10) For return value
 
     vex_conditional_helpers = False
-    syscall_num_offset = 8  # a7(x17) For syscall number (http://www.cs.uwm.edu/classes/cs315/Bacon/Lecture/HTML/ch05s03.html)
+    # a7(x17) For syscall number
+    # According To
+    # http://www.cs.uwm.edu/classes/cs315/Bacon/Lecture/HTML/ch05s03.html
+    syscall_num_offset = 8
     call_pushes_ret = False
     stack_change = -4
 
@@ -54,29 +57,29 @@ class ArchRISCV(Arch):
     #     uc_arch = _unicorn.UC_ARCH_ALL
     #     uc_mode = _unicorn.UC_MODE_LITTLE_ENDIAN
     #     uc_const = None
-    #     uc_prefix = "UC_ALL_"
+    #     uc_prefix = 'UC_ALL_'
     # FIXME: overlap in sw and lw of prologs and epilogs
     function_prologs = {
-        br"[\x00-\xff][\x00-\xf1]\x01\x13"
+        br'[\x00-\xff][\x00-\xf1]\x01\x13',
         # addi sp, sp, xxx
-        # 0b000000000000_00010_000_00010_0010011 0x00010113 
+        # 0b000000000000_00010_000_00010_0010011 0x00010113
         # 0b111111111111_00010_000_00010_0010011 0xfff10113
-        br"[\x00-\xff][\x00-\xf1][\x20-\x2f][\x23-\xa3]"
+        br'[\x00-\xff][\x00-\xf1][\x20-\x2f][\x23-\xa3]'
         # sw xx, xx(sp)
         # 0b0000000_00000_00010_010_00000_0100011 0x00012023
         # 0b1111111_11111_00010_010_11111_0100011 0xfff12fa3
     }
     function_epilogs = {
-        br"[\x00-\xff][\x00-\xf1][\x20-\x2f][\x23-\x83]"
+        br'[\x00-\xff][\x00-\xf1][\x20-\x2f][\x23-\x83]',
         # ld xx, xx(sp)
         # 0b0000000_00000_00010_010_00000_0000011 0x00012003
         # 0b1111111_11111_00010_010_11111_0000011 0xfff12f83
-        br"[\x00-\xff][\x00-\xf1]\x01\x13" # addi sp, sp, xxx
+        br'[\x00-\xff][\x00-\xf1]\x01\x13'  # addi sp, sp, xxx
     }
-    ret_instruction = b"\x00\x00\x80\x67"
+    ret_instruction = b'\x00\x00\x80\x67'
     # jalr x0, x1, 0
     # 0b000000000000_00001_000_00000_1100111
-    nop_instructoin = b"\x00\x00\x00\x13"
+    nop_instructoin = b'\x00\x00\x00\x13'
     # addi x0, x0, 0
     # 0b000000000000_00000_000_00000_0010011
     instruction_alignment = 4
@@ -84,43 +87,77 @@ class ArchRISCV(Arch):
     elf_tls = TLSArchInfo(1, 8, [], [0], [], 0, 0)
 
     register_list = [
-       Register(name="x0", size = 4, alias_names=('zero',), vex_offset=0, default_value=(0,False, 0)),
-       Register(name='x1', size = 4, alias_names=('ra','lr',), general_purpose=True, vex_offset = 4),
-       Register(name='x2', size = 4, alias_names=('sp','bp'), general_purpose=True, default_value=(Arch.initial_sp, True, 'global'), vex_offset=8),
-       Register(name='x3', size = 4, alias_names=('gp',), general_purpose=True, vex_offset=12),
-       Register(name='x4', size = 4, alias_names=('tp',), general_purpose=True, vex_offset=16),
-       Register(name='x5', size = 4, alias_names=('t0',), general_purpose=True, vex_offset=20),
-       Register(name='x6', size = 4, alias_names=('t1',), general_purpose=True, vex_offset=24),
-       Register(name='x7', size = 4, alias_names=('t2',), general_purpose=True, vex_offset=28),
-       Register(name='x8', size = 4, alias_names=('s0','fp'), general_purpose=True, vex_offset=32),
-       Register(name='x9', size = 4, alias_names=('s1',), general_purpose=True, vex_offset=36),
-       Register(name='x10', size = 4, alias_names=('a0',), general_purpose=True, argument=True, vex_offset=40),
-       Register(name='x11', size = 4, alias_names=('a1',), general_purpose=True, argument=True, vex_offset=44),
-       Register(name='x12', size = 4, alias_names=('a2',), general_purpose=True, argument=True, vex_offset=48),
-       Register(name='x13', size = 4, alias_names=('a3',), general_purpose=True, argument=True, vex_offset=52),
-       Register(name='x14', size = 4, alias_names=('a4',), general_purpose=True, argument=True, vex_offset=56),
-       Register(name='x15', size = 4, alias_names=('a5',), general_purpose=True, argument=True, vex_offset=60),
-       Register(name='x16', size = 4, alias_names=('a6',), general_purpose=True, argument=True, vex_offset=64),
-       Register(name='x17', size = 4, alias_names=('a7',), general_purpose=True, argument=True, vex_offset=68),
-       Register(name='x18', size = 4, alias_names=('s2',), general_purpose=True, vex_offset=72),
-       Register(name='x19', size = 4, alias_names=('s3',), general_purpose=True, vex_offset=76),
-       Register(name='x20', size = 4, alias_names=('s4',), general_purpose=True, vex_offset=80),
-       Register(name='x21', size = 4, alias_names=('s5',), general_purpose=True, vex_offset=84),
-       Register(name='x22', size = 4, alias_names=('s6',), general_purpose=True, vex_offset=88),
-       Register(name='x23', size = 4, alias_names=('s7',), general_purpose=True, vex_offset=92),
-       Register(name='x24', size = 4, alias_names=('s8',), general_purpose=True, vex_offset=96),
-       Register(name='x25', size = 4, alias_names=('s9',), general_purpose=True, vex_offset=100),
-       Register(name='x26', size = 4, alias_names=('s10',), general_purpose=True, vex_offset=104),
-       Register(name='x27', size = 4, alias_names=('s11',), general_purpose=True, vex_offset=108),
-       Register(name='x28', size = 4, alias_names=('t3',), general_purpose=True, vex_offset=112),
-       Register(name='x29', size = 4, alias_names=('t4',), general_purpose=True, vex_offset=116),
-       Register(name='x30', size = 4, alias_names=('t5',), general_purpose=True, vex_offset=120),
-       Register(name='x31', size = 4, alias_names=('t6',), general_purpose=True, vex_offset=124),
-       Register(name='ip', alias_names={'pc',}, size=4, vex_offset=128),
+        Register(name='x0', size=4, alias_names=('zero',),
+                 vex_offset=0, default_value=(0, False, 0)),
+        Register(name='x1', size=4, alias_names=('ra', 'lr',),
+                 general_purpose=True, vex_offset=4),
+        Register(name='x2', size=4, alias_names=('sp', 'bp'),
+                 general_purpose=True,
+                 default_value=(Arch.initial_sp, True, 'global'),
+                 vex_offset=8),
+        Register(name='x3', size=4, alias_names=('gp',),
+                 general_purpose=True, vex_offset=12),
+        Register(name='x4', size=4, alias_names=('tp',),
+                 general_purpose=True, vex_offset=16),
+        Register(name='x5', size=4, alias_names=('t0',),
+                 general_purpose=True, vex_offset=20),
+        Register(name='x6', size=4, alias_names=('t1',),
+                 general_purpose=True, vex_offset=24),
+        Register(name='x7', size=4, alias_names=('t2',),
+                 general_purpose=True, vex_offset=28),
+        Register(name='x8', size=4, alias_names=('s0', 'fp'),
+                 general_purpose=True, vex_offset=32),
+        Register(name='x9', size=4, alias_names=('s1',),
+                 general_purpose=True, vex_offset=36),
+        Register(name='x10', size=4, alias_names=('a0',),
+                 general_purpose=True, argument=True, vex_offset=40),
+        Register(name='x11', size=4, alias_names=('a1',),
+                 general_purpose=True, argument=True, vex_offset=44),
+        Register(name='x12', size=4, alias_names=('a2',),
+                 general_purpose=True, argument=True, vex_offset=48),
+        Register(name='x13', size=4, alias_names=('a3',),
+                 general_purpose=True, argument=True, vex_offset=52),
+        Register(name='x14', size=4, alias_names=('a4',),
+                 general_purpose=True, argument=True, vex_offset=56),
+        Register(name='x15', size=4, alias_names=('a5',),
+                 general_purpose=True, argument=True, vex_offset=60),
+        Register(name='x16', size=4, alias_names=('a6',),
+                 general_purpose=True, argument=True, vex_offset=64),
+        Register(name='x17', size=4, alias_names=('a7',),
+                 general_purpose=True, argument=True, vex_offset=68),
+        Register(name='x18', size=4, alias_names=('s2',),
+                 general_purpose=True, vex_offset=72),
+        Register(name='x19', size=4, alias_names=('s3',),
+                 general_purpose=True, vex_offset=76),
+        Register(name='x20', size=4, alias_names=('s4',),
+                 general_purpose=True, vex_offset=80),
+        Register(name='x21', size=4, alias_names=('s5',),
+                 general_purpose=True, vex_offset=84),
+        Register(name='x22', size=4, alias_names=('s6',),
+                 general_purpose=True, vex_offset=88),
+        Register(name='x23', size=4, alias_names=('s7',),
+                 general_purpose=True, vex_offset=92),
+        Register(name='x24', size=4, alias_names=('s8',),
+                 general_purpose=True, vex_offset=96),
+        Register(name='x25', size=4, alias_names=('s9',),
+                 general_purpose=True, vex_offset=100),
+        Register(name='x26', size=4, alias_names=('s10',),
+                 general_purpose=True, vex_offset=104),
+        Register(name='x27', size=4, alias_names=('s11',),
+                 general_purpose=True, vex_offset=108),
+        Register(name='x28', size=4, alias_names=('t3',),
+                 general_purpose=True, vex_offset=112),
+        Register(name='x29', size=4, alias_names=('t4',),
+                 general_purpose=True, vex_offset=116),
+        Register(name='x30', size=4, alias_names=('t5',),
+                 general_purpose=True, vex_offset=120),
+        Register(name='x31', size=4, alias_names=('t6',),
+                 general_purpose=True, vex_offset=124),
+        Register(name='ip', alias_names={'pc', }, size=4, vex_offset=128),
     ]
 
-    got_section_name = ".got"
-    ld_linux_name = "ld-linux-riscv32-ilp32d.so.1"
+    got_section_name = '.got'
+    ld_linux_name = 'ld-linux-riscv32-ilp32d.so.1'
     byte_width = 8
 
 
--- tests/test_riscv.py
+++ tests/test_riscv.py
@@ -2,9 +2,11 @@ from archinfo.arch import Endness
 from nose.tools import raises
 from archinfo import ArchRISCV, ArchError
 
+
 def test_arch_riscv():
     endness = Endness.LE
     assert ArchRISCV(endness)
+
 
 @raises(ArchError)
 def test_arch_riscv_passes():
