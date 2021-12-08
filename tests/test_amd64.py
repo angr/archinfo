34ff9d2e875e25dfe357e421f83eeea01a5c1740
Adersh <36945281+Adersh97@users.noreply.github.com>
2021-12-07 18:56:28 -0700

    Remove nose test dependency (#99)
    * remove nose dependancy
    
    * remove nose
    
    * fix lint error

--- tests/test_amd64.py
+++ tests/test_amd64.py
@@ -1,90 +1,77 @@
+import unittest
+
 from archinfo.arch import Endness
-from nose.tools import raises
 from archinfo import ArchAMD64, ArchError
-import nose.tools
-
-try:
-    import capstone as _capstone
-except ImportError:
-    _capstone = None
 
 
-def test_arch_amd64():
-    endness = Endness.LE
-    assert ArchAMD64(endness)
+class TestAmd64(unittest.TestCase):
+    def test_arch_amd64(self):
+        endness = Endness.LE
+        assert ArchAMD64(endness)
 
+    def test_arch_amd64_passes(self):
+        with self.assertRaises(ArchError):
+            endness = Endness.BE
+            ArchAMD64(endness)
 
-@raises(ArchError)
-def test_arch_amd64_passes():
-    endness = Endness.BE
-    ArchAMD64(endness)
+    def test_capstone_x86_syntax(self):
+        inst_1 = ArchAMD64(endness=Endness.LE)
+        assert inst_1.capstone_x86_syntax is None
+        inst_1.capstone_x86_syntax = "intel"
+        assert inst_1.capstone_x86_syntax == "intel"
+        inst_1.capstone_x86_syntax = "at&t"
+        assert inst_1.capstone_x86_syntax == "at&t"
 
+    # Test raises one of expected exceptions to pass.
+    def test_capstone_x86_syntax_fails_1(self):
+        with self.assertRaises(ArchError):
+            inst_1 = ArchAMD64(endness=Endness.LE)
+            inst_1.capstone_x86_syntax = "at&"
+            assert inst_1.capstone_x86_syntax
 
-def test_capstone_x86_syntax():
-    inst_1 = ArchAMD64(endness=Endness.LE)
-    nose.tools.assert_is_none(inst_1.capstone_x86_syntax)
-    inst_1.capstone_x86_syntax = 'intel'
-    nose.tools.assert_equal(inst_1.capstone_x86_syntax, 'intel')
-    inst_1.capstone_x86_syntax = 'at&t'
-    nose.tools.assert_equal(inst_1.capstone_x86_syntax, 'at&t')
+    def test_capstone_x86_syntax_fails_2(self):
+        with self.assertRaises(ArchError):
+            inst_1 = ArchAMD64(endness=Endness.LE)
+            inst_1.capstone_x86_syntax = "int"
+            assert inst_1.capstone_x86_syntax
 
+    def test_keystone_x86_syntax(self):
+        inst_1 = ArchAMD64(endness=Endness.LE)
+        assert inst_1.keystone_x86_syntax is None
+        inst_1.keystone_x86_syntax = "intel"
+        assert inst_1.keystone_x86_syntax == "intel"
+        inst_1.keystone_x86_syntax = "at&t"
+        assert inst_1.keystone_x86_syntax == "at&t"
+        inst_1.keystone_x86_syntax = "nasm"
+        assert inst_1.keystone_x86_syntax == "nasm"
+        inst_1.keystone_x86_syntax = "masm"
+        assert inst_1.keystone_x86_syntax == "masm"
+        inst_1.keystone_x86_syntax = "gas"
+        assert inst_1.keystone_x86_syntax == "gas"
+        inst_1.keystone_x86_syntax = "radix16"
+        assert inst_1.keystone_x86_syntax == "radix16"
 
-# Test raises one of expected exceptions to pass.
-@raises(ArchError)
-def test_capstone_x86_syntax_fails_1():
-    inst_1 = ArchAMD64(endness=Endness.LE)
-    inst_1.capstone_x86_syntax = 'at&'
-    assert inst_1.capstone_x86_syntax
+    # Test raises one of expected exceptions to pass.
+    def test_keystone_x86_syntax_fails_1(self):
+        with self.assertRaises(ArchError):
+            inst_1 = ArchAMD64(endness=Endness.LE)
+            inst_1.keystone_x86_syntax = "inte"
+            assert inst_1.keystone_x86_syntax
 
+    def test_keystone_x86_syntax_fails_2(self):
+        with self.assertRaises(ArchError):
+            inst_1 = ArchAMD64(endness=Endness.LE)
+            inst_1.keystone_x86_syntax = "at"
+            assert inst_1.keystone_x86_syntax
 
-@raises(ArchError)
-def test_capstone_x86_syntax_fails_2():
-    inst_1 = ArchAMD64(endness=Endness.LE)
-    inst_1.capstone_x86_syntax = 'int'
-    assert inst_1.capstone_x86_syntax
+    def test_keystone_x86_syntax_fails_3(self):
+        with self.assertRaises(ArchError):
+            inst_1 = ArchAMD64(endness=Endness.LE)
+            inst_1.keystone_x86_syntax = "na"
+            assert inst_1.keystone_x86_syntax
 
-
-def test_keystone_x86_syntax():
-    inst_1 = ArchAMD64(endness=Endness.LE)
-    nose.tools.assert_is_none(inst_1.keystone_x86_syntax)
-    inst_1.keystone_x86_syntax = 'intel'
-    nose.tools.assert_equal(inst_1.keystone_x86_syntax, 'intel')
-    inst_1.keystone_x86_syntax = 'at&t'
-    nose.tools.assert_equal(inst_1.keystone_x86_syntax, 'at&t')
-    inst_1.keystone_x86_syntax = 'nasm'
-    nose.tools.assert_equal(inst_1.keystone_x86_syntax, 'nasm')
-    inst_1.keystone_x86_syntax = 'masm'
-    nose.tools.assert_equal(inst_1.keystone_x86_syntax, 'masm')
-    inst_1.keystone_x86_syntax = 'gas'
-    nose.tools.assert_equal(inst_1.keystone_x86_syntax, 'gas')
-    inst_1.keystone_x86_syntax = 'radix16'
-    nose.tools.assert_equal(inst_1.keystone_x86_syntax, 'radix16')
-
-
-# Test raises one of expected exceptions to pass.
-@raises(ArchError)
-def test_keystone_x86_syntax_fails_1():
-    inst_1 = ArchAMD64(endness=Endness.LE)
-    inst_1.keystone_x86_syntax = 'inte'
-    assert inst_1.keystone_x86_syntax
-
-
-@raises(ArchError)
-def test_keystone_x86_syntax_fails_2():
-    inst_1 = ArchAMD64(endness=Endness.LE)
-    inst_1.keystone_x86_syntax = 'at'
-    assert inst_1.keystone_x86_syntax
-
-
-@raises(ArchError)
-def test_keystone_x86_syntax_fails_3():
-    inst_1 = ArchAMD64(endness=Endness.LE)
-    inst_1.keystone_x86_syntax = 'na'
-    assert inst_1.keystone_x86_syntax
-
-
-@raises(ArchError)
-def test_keystone_x86_syntax_fails_4():
-    inst_1 = ArchAMD64(endness=Endness.LE)
-    inst_1.keystone_x86_syntax = 'ma'
-    assert inst_1.keystone_x86_syntax
+    def test_keystone_x86_syntax_fails_4(self):
+        with self.assertRaises(ArchError):
+            inst_1 = ArchAMD64(endness=Endness.LE)
+            inst_1.keystone_x86_syntax = "ma"
+            assert inst_1.keystone_x86_syntax
