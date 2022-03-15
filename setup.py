0168a90a8f6a439b14baca333da933f59be6d838
Kevin Phoenix <kevin@kphoenix.us>
2022-03-15 15:03:47 -0700

    Add pyproject.toml

--- setup.py
+++ setup.py
@@ -1,14 +1,4 @@
-try:
-    from setuptools import setup
-    from setuptools import find_packages
-    packages = find_packages()
-except ImportError:
-    from distutils.core import setup
-    import os
-    packages = [x.strip('./').replace('/','.') for x in os.popen('find . -name "__init__.py" | xargs -n1 dirname').read().strip().split('\n')]
-
-if bytes is str:
-    raise Exception("This module is designed for python 3 only. Please install an older version to use python 2.")
+from setuptools import setup, find_packages
 
 setup(
     name='archinfo',
