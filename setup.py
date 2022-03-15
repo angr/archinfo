5aba45884310020a854b1c9d00a7041227174751
Kevin Phoenix <kevin@kphoenix.us>
2022-03-15 16:31:10 -0700

    Add pyproject.toml (#110)
    * Add pyproject.toml
    
    * Fix find_packages()

--- setup.py
+++ setup.py
@@ -1,20 +1,10 @@
-try:
-    from setuptools import setup
-    from setuptools import find_packages
-    packages = find_packages()
-except ImportError:
-    from distutils.core import setup
-    import os
-    packages = [x.strip('./').replace('/','.') for x in os.popen('find . -name "__init__.py" | xargs -n1 dirname').read().strip().split('\n')]
-
-if bytes is str:
-    raise Exception("This module is designed for python 3 only. Please install an older version to use python 2.")
+from setuptools import setup, find_packages
 
 setup(
     name='archinfo',
     version='9.1.gitrolling',
     python_requires='>=3.6',
-    packages=packages,
+    packages=find_packages(),
     package_data={'archinfo': ['py.typed']},
     url="https://github.com/angr/archinfo",
 )
