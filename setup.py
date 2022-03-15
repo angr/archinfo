480742404b2ac85d5a01e92ae7df2e80f24cbbdc
Kevin Phoenix <kevin@kphoenix.us>
2022-03-15 15:09:59 -0700

    Fix find_packages()

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='archinfo',
     version='9.1.gitrolling',
     python_requires='>=3.6',
-    packages=packages,
+    packages=find_packages(),
     package_data={'archinfo': ['py.typed']},
     url="https://github.com/angr/archinfo",
 )
