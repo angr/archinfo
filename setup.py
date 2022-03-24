9832609647e6e6d5400eb1589338dd318407a914
Kevin Phoenix <kevin@kphoenix.us>
2022-03-23 19:06:42 -0700

    Update version to 9.2.0.dev0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='archinfo',
-    version='9.1.gitrolling',
+    version='9.2.0.dev0',
     python_requires='>=3.6',
     packages=find_packages(),
     package_data={'archinfo': ['py.typed']},
