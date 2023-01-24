62c0e4330766278ff4581b83ad77c20060536403
Matt Borgerson <contact@mborgerson.com>
2023-01-24 00:08:10 -0700

    ArchPcode: Add a few test cases

---
+++ tests/test_pcode.py
@@ -0,0 +1,31 @@
+# pylint:disable=missing-class-docstring,no-self-use
+import unittest
+import pickle
+
+try:
+    import pypcode
+except ImportError:
+    pypcode = None
+
+from archinfo.arch import Endness
+from archinfo import ArchPcode, ArchError
+
+
+@unittest.skipUnless(pypcode is not None, "pypcode not installed")
+class TestArchPcode(unittest.TestCase):
+    def test_arch_68000(self):
+        arch = ArchPcode("68000:BE:32:default")
+        assert arch.instruction_endness == Endness.BE
+        assert arch.bits == 32
+
+    def test_arch_bad_langid(self):
+        with self.assertRaises(ArchError):
+            ArchPcode("invalid")
+
+    def test_pickle(self):
+        arch = ArchPcode("68000:BE:32:default")
+        pickle.dumps(arch)
+
+
+if __name__ == "__main__":
+    unittest.main()
