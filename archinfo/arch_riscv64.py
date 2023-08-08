43944573c3ed65e4a3bc2a9daacece192c16494d
Evelyn Vu <evu@draper.com>
2023-08-08 14:57:30 -0400

    Reformatted function to check for RISCV arch to pass pre-commit

--- archinfo/arch_riscv64.py
+++ archinfo/arch_riscv64.py
@@ -17,8 +17,10 @@ try:
 except ImportError:
     _unicorn = None
 
+
 def is_riscv_arch(a):
     return a.name.startswith("RISCV")
+
 
 class ArchRISCV64(Arch):
     def __init__(self, endness=Endness.LE):
