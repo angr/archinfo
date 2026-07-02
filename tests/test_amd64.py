fdfbd224b5835fbdce014b1e8cb752aa11821377
tedanvosin <vedusoni@gmail.com>
2026-07-02 13:46:14 -0700

    remove amd64 syntax tests

--- tests/test_amd64.py
+++ tests/test_amd64.py
@@ -35,43 +35,8 @@ class TestAmd64(unittest.TestCase):
             inst_1.capstone_x86_syntax = "int"
             assert inst_1.capstone_x86_syntax
 
-    def test_keystone_x86_syntax(self):
+    def test_assembler_support(self):
         inst_1 = ArchAMD64(endness=Endness.LE)
-        assert inst_1.keystone_x86_syntax is None
-        inst_1.keystone_x86_syntax = "intel"
-        assert inst_1.keystone_x86_syntax == "intel"
-        inst_1.keystone_x86_syntax = "at&t"
-        assert inst_1.keystone_x86_syntax == "at&t"
-        inst_1.keystone_x86_syntax = "nasm"
-        assert inst_1.keystone_x86_syntax == "nasm"
-        inst_1.keystone_x86_syntax = "masm"
-        assert inst_1.keystone_x86_syntax == "masm"
-        inst_1.keystone_x86_syntax = "gas"
-        assert inst_1.keystone_x86_syntax == "gas"
-        inst_1.keystone_x86_syntax = "radix16"
-        assert inst_1.keystone_x86_syntax == "radix16"
-
-    # Test raises one of expected exceptions to pass.
-    def test_keystone_x86_syntax_fails_1(self):
-        with self.assertRaises(ArchError):
-            inst_1 = ArchAMD64(endness=Endness.LE)
-            inst_1.keystone_x86_syntax = "inte"
-            assert inst_1.keystone_x86_syntax
-
-    def test_keystone_x86_syntax_fails_2(self):
-        with self.assertRaises(ArchError):
-            inst_1 = ArchAMD64(endness=Endness.LE)
-            inst_1.keystone_x86_syntax = "at"
-            assert inst_1.keystone_x86_syntax
-
-    def test_keystone_x86_syntax_fails_3(self):
-        with self.assertRaises(ArchError):
-            inst_1 = ArchAMD64(endness=Endness.LE)
-            inst_1.keystone_x86_syntax = "na"
-            assert inst_1.keystone_x86_syntax
-
-    def test_keystone_x86_syntax_fails_4(self):
-        with self.assertRaises(ArchError):
-            inst_1 = ArchAMD64(endness=Endness.LE)
-            inst_1.keystone_x86_syntax = "ma"
-            assert inst_1.keystone_x86_syntax
+        assert inst_1.assembler_support
+        assert inst_1.nyxstone_triple == "x86_64-linux-gnu"
+        assert inst_1.asm("nop") == b"\x90"
